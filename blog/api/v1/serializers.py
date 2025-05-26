08744d93f9a583474ce7e4973c2a1c68c6e3f3eb
Always-Learningeek <myrealemail1400@gmail.com>
2025-05-26 09:11:10 -0700

    methods added to blog app
    Now we have POST and Put methods in blog post and post details.

--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
-
 from blog.models import Post
 
 ''' 
 class PostSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=100)
+    
+
+    def create(self, validated_data):
+        return Post.objects.create()
 '''
+
 
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'status']
+        fields = ['id','author','title','content','category','status','created_date','published_date']
+
--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -6,25 +6,27 @@ from ...models import Post
 from django.shortcuts import get_object_or_404
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def api_v1_post_list(request):
-    posts = Post.objects.filter(status=True)
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+    if request.method == "GET" :
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def api_v1_post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk, status=True)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
-
-    # another way
-    ''' 
-    try :
-        post = Post.objects.get(pk=pk)
+    if request.method == "GET" :
         serializer = PostSerializer(post)
         return Response(serializer.data)
-    except Post.DoesNotExist:
-        return Response({"detail : Not exist ! "}, status=status.HTTP_404_NOT_FOUND)
-    '''
+    elif request.method == "PUT" :
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
